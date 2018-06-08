5924e9107abc48e751b83dc22da9d4f4d3d2715c
Divyateja Pasupuleti <divyateja2004@gmail.com>
2018-06-08 14:00:10 +0530

    avc

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,12 +1,13 @@
 //  Menu
 
-const openSideMenu = () =>{
-	document.querySelector("#side-menu").style.width = "250px";
-	document.querySelector("#main").style.marginLeft = "250px";
-};
+// const openSideMenu = () =>{
+// 	document.querySelector("#side-menu").style.width = "250px";
+// };
 
-const closeSideMenu = () =>{
-	document.querySelector("#side-menu").style.width = "0";
-	document.querySelector("#main").style.marginLeft = "0";
-};
+// const closeSideMenu = () =>{
+// 	document.querySelector("#side-menu").style.width = "0";
+// };
 
+function sidemenu(){
+	document.querySelector("#side-menu").classList.toggle("side-menu");
+}
