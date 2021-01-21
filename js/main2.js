222aedf1af91aebfe27916510e3013f01aa2c87e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2021-01-22 00:13:07 +0530

    up

--- js/main2.js
+++ js/main2.js
@@ -16,3 +16,13 @@
         }
     });
 })(jQuery);
+
+ //===== Progress Bar
+    
+    if($('.progress-line').length){
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width',percent+'%');
+        },{accY: 0});
+    }
